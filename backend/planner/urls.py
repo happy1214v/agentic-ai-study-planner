b1ea27c6018e68e29e8128691c3bf3d6fca56a32
Siddharth Verma <happy1214v@gmail.com>
2026-08-08 21:41:25 +0530

    Initial Agentic AI project

--- backend/planner/urls.py
+++ backend/planner/urls.py
@@ -3,12 +3,13 @@ from .views import (
     SubjectListView,
     StudyPlanView,
     ProgressView,
-    AdjustPlanView
+    AdjustPlanView,
+    DashboardView
 )
 urlpatterns = [
     path("subjects/", SubjectListView.as_view(), name="subjects"),
     path("study-plan/", StudyPlanView.as_view(), name="study-plan"),
     path("progress/", ProgressView.as_view(), name="progress"),
     path("adjust-plan/", AdjustPlanView.as_view(), name="adjust-plan"),
-    
+    path("dashboard/", DashboardView.as_view(), name="dashboard"),
 ]
--- backend/planner/views.py
+++ backend/planner/views.py
@@ -6,7 +6,7 @@ from rest_framework import status
 from .models import Subject, StudyProgress
 from .serializers import SubjectSerializer, StudyProgressSerializer
 from .ai_service import generate_study_plan ,adjust_study_plan
-
+from datetime import date
 
 
 class SubjectListView(generics.ListAPIView):
@@ -119,4 +119,61 @@ class AdjustPlanView(APIView):
             "subject": subject.name,
             "exam_date": str(subject.exam_date),
             "adjusted_plan": adjusted_plan
+        })
+
+
+
+
+class DashboardView(APIView):
+
+    def get(self, request):
+
+        subjects = Subject.objects.all()
+
+        total_subjects = subjects.count()
+
+        today = date.today()
+
+        today_progress = StudyProgress.objects.filter(
+            study_date=today
+        )
+
+        today_planned_hours = sum(
+            item.planned_hours
+            for item in today_progress
+        )
+
+        today_completed_hours = sum(
+            item.completed_hours
+            for item in today_progress
+        )
+
+        if today_planned_hours > 0:
+            progress_percentage = round(
+                (today_completed_hours / today_planned_hours) * 100,
+                2
+            )
+        else:
+            progress_percentage = 0
+
+        upcoming_subject = subjects.filter(
+            exam_date__gte=today
+        ).order_by("exam_date").first()
+
+        if upcoming_subject:
+            next_exam = str(upcoming_subject.exam_date)
+            days_remaining = (
+                upcoming_subject.exam_date - today
+            ).days
+        else:
+            next_exam = None
+            days_remaining = None
+
+        return Response({
+            "total_subjects": total_subjects,
+            "today_planned_hours": today_planned_hours,
+            "today_completed_hours": today_completed_hours,
+            "progress_percentage": progress_percentage,
+            "next_exam": next_exam,
+            "days_remaining": days_remaining
         })
